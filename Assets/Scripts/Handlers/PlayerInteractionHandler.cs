af46d43c601cb66020745c08e403b435e482a12b
BrandonLeho <brandoncleho@gmail.com>
2025-09-05 09:03:56 -0700

    mouse cursor network

--- Assets/Scripts/Handlers/PlayerInteractionHandler.cs
+++ Assets/Scripts/Handlers/PlayerInteractionHandler.cs
@@ -1,20 +1,27 @@
-using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.InputSystem;
 
 namespace SteamLobbySpace
 {
     public class PlayerInteractionHandler : NetworkBehaviour
     {
+        [SerializeField] float zDepthFromCamera = 10f;
+
+        [ClientCallback]
+        void OnEnable() { if (isLocalPlayer) Cursor.visible = false; }
+        [ClientCallback]
+        void OnDisable() { if (isLocalPlayer) Cursor.visible = true; }
+
+        [ClientCallback]
         void Update()
         {
-            if (isLocalPlayer)
-            {
-                float h = Input.GetAxis("Horizontal");
-                float v = Input.GetAxis("Vertical");
-                Vector3 playerMovement = new Vector3(h * 0.25f, v * 0.25f, 0);
-                transform.position = transform.position + playerMovement;
-            }
+            if (!isLocalPlayer) return;
+
+            Vector2 sp = Mouse.current.position.ReadValue();   // New Input System
+            var mp = new Vector3(sp.x, sp.y, zDepthFromCamera);
+            Vector3 world = Camera.main.ScreenToWorldPoint(mp);
+            transform.position = new Vector3(world.x, world.y, 0f);
         }
     }
 }
