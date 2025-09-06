19c4f0d80dab7a704bc551951a0d56f3b32b1ff0
BrandonLeho <brandoncleho@gmail.com>
2025-09-05 23:36:11 -0700

    asdgiusdhagols

---
+++ Assets/Scripts/Handlers/PlayerLobbyHandlerGame.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+using TMPro;
+
+namespace SteamLobbySpace
+{
+    public class PlayerLobbyHandlerGame : NetworkBehaviour
+    {
+        public TextMeshProUGUI nameText;
+    }
+}
+
