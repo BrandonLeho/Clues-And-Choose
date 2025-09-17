43a72a14ad332dbd6566d2f2e157a05e6381a750
BrandonLeho <brandoncleho@gmail.com>
2025-09-16 19:25:26 -0700

    vauhsiue

--- Assets/Scripts/Board/Coins/CoinDragSync.cs
+++ Assets/Scripts/Board/Coins/CoinDragSync.cs
@@ -4,30 +4,68 @@ using UnityEngine;
 [RequireComponent(typeof(NetworkCoin))]
 public class CoinDragSync : NetworkBehaviour
 {
+    [Header("Streaming")]
     [SerializeField] float sendInterval = 0.02f;
+    [SerializeField] float minSendDelta = 0.0009f;
     [SerializeField] float dragZ = 0f;
+
+    [Header("Smoothing for non-owners")]
+    [SerializeField] float lerpSpeed = 20f;
+    [SerializeField] float snapIfFar = 0.5f;
 
     NetworkCoin _coin;
     float _lastSend;
     bool _streaming;
 
-    void Awake() => _coin = GetComponent<NetworkCoin>();
+    Vector3 _targetPos;
+    bool _hasTarget;
+
+    void Awake()
+    {
+        _coin = GetComponent<NetworkCoin>();
+        _targetPos = transform.position;
+    }
 
     public void BeginLocalDrag()
     {
         if (_coin.IsLocalOwner()) _streaming = true;
     }
+
     public void EndLocalDrag() => _streaming = false;
 
-    void Update()
+    void LateUpdate()
     {
-        if (!_streaming || !_coin.IsLocalOwner()) return;
-        if (Time.unscaledTime - _lastSend < sendInterval) return;
-        _lastSend = Time.unscaledTime;
+        if (_streaming && _coin.IsLocalOwner())
+        {
+            if (Time.unscaledTime - _lastSend >= sendInterval)
+            {
+                _lastSend = Time.unscaledTime;
 
-        var p = transform.position; p.z = dragZ;
-        CmdMove(p);
+                var p = transform.position; p.z = dragZ;
+
+                if ((_lastSent - p).sqrMagnitude > minSendDelta)
+                {
+                    _lastSent = p;
+                    CmdMove(p);
+                }
+            }
+            return;
+        }
+
+        if (!_coin.IsLocalOwner() && _hasTarget)
+        {
+            if ((transform.position - _targetPos).sqrMagnitude > snapIfFar * snapIfFar)
+            {
+                transform.position = _targetPos;
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, _targetPos, Time.unscaledDeltaTime * lerpSpeed);
+            }
+        }
     }
+
+    Vector3 _lastSent;
 
     [Command(requiresAuthority = false)]
     void CmdMove(Vector3 pos, NetworkConnectionToClient sender = null)
@@ -36,6 +74,7 @@ public class CoinDragSync : NetworkBehaviour
         if (_coin.ownerNetId != sender.identity.netId) return;
 
         pos.z = dragZ;
+
         transform.position = pos;
 
         RpcMoved(pos);
@@ -45,6 +84,9 @@ public class CoinDragSync : NetworkBehaviour
     void RpcMoved(Vector3 pos)
     {
         pos.z = dragZ;
-        transform.position = pos;
+        _targetPos = pos;
+        _hasTarget = true;
+
+        transform.position = Vector3.Lerp(transform.position, _targetPos, 0.25f);
     }
 }
