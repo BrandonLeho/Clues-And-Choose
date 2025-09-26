548f9d3fe1fe4fc2a01c718dd023ead771387ec2
BrandonLeho <brandoncleho@gmail.com>
2025-09-25 17:25:01 -0700

    Added cursor icon change for the coins

---
+++ Assets/Scripts/Board/Coins/CoinCursorBridge.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(CoinDragHandler))]
+public class CoinCursorBridge : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    CoinDragHandler _drag;
+    ICoinDragPermission[] _permGuards;
+    NetworkCoin _net;
+    CoinRejectionFeedback _reject;
+
+    bool _isPointerOver;
+    bool _isDragging;
+
+    void Awake()
+    {
+        _drag = GetComponent<CoinDragHandler>();
+        _permGuards = GetComponents<ICoinDragPermission>();
+        _net = GetComponent<NetworkCoin>();
+        _reject = GetComponent<CoinRejectionFeedback>();
+
+        _drag.onPickUp.AddListener(OnPickedUp);
+        _drag.onDrop.AddListener(OnDropped);
+    }
+
+    void OnDestroy()
+    {
+        _drag.onPickUp.RemoveListener(OnPickedUp);
+        _drag.onDrop.RemoveListener(OnDropped);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _isPointerOver = true;
+        if (_isDragging) return;
+
+        if (CanBeginDragLikeHandler())
+            CursorControllerModule.Instance.SetToMode(CursorControllerModule.ModeOfCursor.Draggable);
+        else
+            CursorControllerModule.Instance.SetToMode(CursorControllerModule.ModeOfCursor.Default);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _isPointerOver = false;
+        if (_isDragging) return;
+
+        CursorControllerModule.Instance.SetToMode(CursorControllerModule.ModeOfCursor.Default);
+    }
+
+    void OnPickedUp()
+    {
+        _isDragging = true;
+        CursorControllerModule.Instance.SetToMode(CursorControllerModule.ModeOfCursor.Dragging);
+    }
+
+    void OnDropped()
+    {
+        _isDragging = false;
+
+        if (_isPointerOver && CanBeginDragLikeHandler())
+            CursorControllerModule.Instance.SetToMode(CursorControllerModule.ModeOfCursor.Draggable);
+        else
+            CursorControllerModule.Instance.SetToMode(CursorControllerModule.ModeOfCursor.Default);
+    }
+
+    bool CanBeginDragLikeHandler()
+    {
+        if (_reject != null && _reject.IsPlaying) return false;
+        if (_net != null && !_net.IsLocalOwner()) return false;
+
+        if (_permGuards != null)
+        {
+            for (int i = 0; i < _permGuards.Length; i++)
+                if (_permGuards[i] != null && !_permGuards[i].CanBeginDrag())
+                    return false;
+        }
+        return true;
+    }
+
+    void OnDisable()
+    {
+        if (CursorControllerModule.Instance != null)
+            CursorControllerModule.Instance.SetToMode(CursorControllerModule.ModeOfCursor.Default);
+    }
+}
