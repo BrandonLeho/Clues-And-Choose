2c0ccb2ab97cfe458c9bc63ce2c544d6f9890964
BrandonLeho <brandoncleho@gmail.com>
2025-09-28 22:30:54 -0700

    Coin lock overlay

--- Assets/Scripts/Board/Coins/CoinRoundLockManager.cs
+++ Assets/Scripts/Board/Coins/CoinRoundLockManager.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 public sealed class CoinRoundLockManager : MonoBehaviour
 {
     static CoinRoundLockManager _instance;
-    readonly static HashSet<CoinLockedState> _locks = new HashSet<CoinLockedState>();
+    static readonly HashSet<CoinLockedState> _locks = new HashSet<CoinLockedState>();
+
+    public static bool IsLockedGlobally { get; private set; } = true;
+    public static event System.Action<bool> onGlobalLockStateChanged;
 
     public static void Register(CoinLockedState l) { if (l) _locks.Add(l); }
     public static void Unregister(CoinLockedState l) { if (l) _locks.Remove(l); }
@@ -13,17 +16,27 @@ public sealed class CoinRoundLockManager : MonoBehaviour
     {
         if (_instance && _instance != this) { Destroy(gameObject); return; }
         _instance = this;
+        SetGlobal(true, invokeEvenIfSame: false);
     }
 
     [ContextMenu("Unlock All Coins")]
     public void UnlockAllCoins()
     {
         foreach (var l in _locks) if (l) l.Unlock();
+        SetGlobal(false);
     }
 
     [ContextMenu("Lock All Coins")]
     public void LockAllCoins()
     {
         foreach (var l in _locks) if (l) l.Lock();
+        SetGlobal(true);
+    }
+
+    void SetGlobal(bool locked, bool invokeEvenIfSame = true)
+    {
+        if (IsLockedGlobally == locked && !invokeEvenIfSame) return;
+        IsLockedGlobally = locked;
+        onGlobalLockStateChanged?.Invoke(locked);
     }
 }
